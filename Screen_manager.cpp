11666d400e0578c8f9b64cd5cf2ce21f5de95d8d
junggernaut <seungan1419@gmail.com>
2023-06-22 23:35:27 +0900

    enemy 1 shooting print error fix

--- Screen_manager.cpp
+++ Screen_manager.cpp
@@ -62,7 +62,6 @@ void Screen_manager::print_share() {
           if (iter->y == (*currentUnit)->y && iter->x == (*currentUnit)->x) {
             (*currentUnit)->shot(iter->damage);
             if ((*currentUnit)->hp <= 0) {
-              move((*currentUnit)->y, (*currentUnit)->x);
               this->units.erase(currentUnit);
             }
             this->my_plane.bullet.erase(iter);
